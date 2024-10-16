c68f185d2fc36641360814ddf3ea197e97c1d369
FS <francesco9721@gmail.com>
2024-10-16 23:27:30 +0200

    Neuer Zwischenstand

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.getElementById('subBtn').addEventListener('click', showLifeTime);
 
 function showLifeTime() {
-    let randomNum = Math.random();
+    let randomNum = Math.random() * 90;
     alert('dU HaSt NoCh ' + randomNum + ' JaHrE Zu LeBeN');
 }
