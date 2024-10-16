14c2605b3ee3c243975ff470dacd8b0412368721
FS <francesco9721@gmail.com>
2024-10-16 22:52:45 +0200

    Kurzer Zwischenstand

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('subBtn').addEventListener('click', showLifeTime);
+
+function showLifeTime() {
+    let randomNum = Math.random();
+    alert('dU HaSt NoCh ' + randomNum + ' JaHrE Zu LeBeN');
+}
